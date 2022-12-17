7e63fac11f7b5aa1dd4a9279932a3d79238753a5
holodorum <bart@Barts-MacBook-Pro.local>
2022-12-17 10:13:51 -0600

    add comment with todo

--- main.py
+++ main.py
@@ -16,3 +16,6 @@ athlete_1_extractor.soupify_content()
 athlete_1_extractor.get_name()
 print(athlete_1_extractor.get_assigned_matches())
 print(athlete_1_extractor.get_upcoming_matches())
+
+#If I want to finish this, just iterate over the names that you want to scrape.
+#Create a pretty print class. And stuff is done. 
